b71a3492829e5929abcff47183e99060dd3d32c7
willemurkyiex-sketch <willemurkyiex@gmail.com>
2026-02-03 14:23:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const heartsBg = document.querySelector(".background-hearts");
+
+let scale = 1;
+
+noBtn.addEventListener("click", () => {
+    scale += 0.25;
+    yesBtn.style.transform = `scale(${scale})`;
+
+    noBtn.style.position = "absolute";
+    noBtn.style.left = Math.random() * 70 + "%";
+    noBtn.style.top = Math.random() * 70 + "%";
+});
+
+yesBtn.addEventListener("click", () => {
+    document.body.innerHTML = `
+        <div class="card">
+            <h1 style="color:#ff4d88;">🎉 JAAAAA!! 🎉</h1>
+            <p>Ik ben zo blij 😍💖</p>
+        </div>
+    `;
+});
+
+// zwevende hartjes
+function createHeart() {
+    const heart = document.createElement("span");
+    heart.innerHTML = Math.random() > 0.5 ? "💗" : "😻";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 14 + "px";
+    heartsBg.appendChild(heart);
+
+    setTimeout(() => heart.remove(), 6000);
+}
+
+setInterval(createHeart, 300);
